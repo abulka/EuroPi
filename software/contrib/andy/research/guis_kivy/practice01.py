b50de43828665cf919accac2eb2b8d88272af2b0
Andy Bulka <abulka@gmail.com>
2022-12-16 21:48:02 +1100

    research: colours

--- software/contrib/andy/research/guis_kivy/practice01.py
+++ software/contrib/andy/research/guis_kivy/practice01.py
@@ -1,15 +1,59 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
+# Window.clearcolor = (1, 1, 1, 1)
+Window.clearcolor = 'Red'
 
 kv = '''
 BoxLayout:
+    # canvas.before:
+    #     Color:
+    #         rgba: 255, 255, 50, 1
+    #     Rectangle:
+    #         pos: self.pos
+    #         size: self.size
     orientation: 'vertical'
+    padding: 50
+    spacing: 100
+
+    GridLayout:
+        cols: 4
+        padding: 50
+        spacing: 20
+        Label:
+            text: 'Digital Input'
+        CheckBox:
+            id: 'din'
+            height: '48dp'
+            colour: 'red'
+        Label:
+            text: 'Analogue Input'
+        CheckBox:
+            id: 'ain'
+            height: '48dp'
+    # Canvas rectangle
+    BoxLayout:
+        orientation: 'horizontal'
+        padding: 50
+        spacing: 100        
+        Slider:
+            id: 'k1'
+        Slider:
+            id: 'k2'
+    BoxLayout:
+        orientation: 'horizontal'
+        padding: 50
+        spacing: 100        
+        Button:
+            text: 'b1'
+        Button:
+            text: 'b2'
+
     Label:
         text: 'Hello'+' World!'
 
-<Label>
-    text: 'Hello'+' World!'
+
 '''
 
 # kv = '''

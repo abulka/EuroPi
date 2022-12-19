f304bcecfc59b3346041028d34263552701a7c06
Andy Bulka <abulka@gmail.com>
2022-12-19 13:27:07 +1100

    feat: draw logo in regular kivy version

--- software/contrib/andy/master_clock_kivy_gui.py
+++ software/contrib/andy/master_clock_kivy_gui.py
@@ -2,15 +2,24 @@
 in asyncio event loop as just another async coroutine.
 '''
 import asyncio
-
+from PIL import Image, ImageOps
+from io import BytesIO
 from kivy.app import async_runTouchApp
 from kivy.lang.builder import Builder
 from master_clock import MasterClockInner
 from europi import cvs, get_cvs_snapshot_msg, oled, bootsplash, b1, b2, din, k1, k2
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.graphics.texture import Texture
+from kivy.graphics import Rectangle
+from kivy.core.image import Image as CoreImage
+from kivy.core.text import Label as CoreLabel
+from europi_simulator_util import convert_to_xbm
+from europi import FrameBuffer, MONO_HLSB
 
 kv = '''
+EuroPiLayout:
+
 #:set leds dp(20)
 <CanvasCvIn>:
     canvas:
@@ -47,10 +56,13 @@ kv = '''
         CanvasCvIn:
         CanvasCvIn:
     BoxLayout:
-        size_hint: 0.8, 0.2
+        size_hint: 0.8, 0.5
         pos_hint: {'center_x':0.5}
         Display:
             id: disp
+            on_touch_down:
+                # self.clear()
+                self.drawLogo()
     BoxLayout:
         orientation: 'horizontal'
         padding: '30dp', '20dp'
@@ -83,47 +95,47 @@ kv = '''
         CanvasLed:
         CanvasLed:
         CanvasLed:            
-BoxLayout:
-    orientation: 'vertical'
-    EuroPiLayout:
-        size_hint: 1, 5
+# BoxLayout:
+#     orientation: 'vertical'
+#     EuroPiLayout:
+#         size_hint: 1, 5
 
-    # Debug Area
-    BoxLayout:
-        orientation: 'horizontal'
-        padding: '30dp', '20dp'
-        spacing: '20dp'
-        Button:
-            text: 'Exit'
-            on_press:
-                # app.stop()
-                exit()
-        Button:
-            text: 'Draw Logo'
-            # on_press: app.root.ids.disp.drawLogo()
-        Button:
-            text: 'Clear Display'
-            on_press:
-                on_press: root.ids.disp.clear()
-        Button:
-            text: 'DUMP'
-            on_press:
-                # label.text = 'The button was pressed'
-                # print('app', app) # CRASH
-                # print('app.root', app.root) # CRASH
-                print('root.ids', root.ids)
-                print('self.ids', self.ids)
-                print('self.parent.ids', self.parent.ids)
-            on_release:
-                # label.text = 'The button was released'
-                print('The button was released')
-        Button:
-            id: btn
-            text: 'Press me'
-    BoxLayout:
-        Label:
-            id: label
-            text: 'Button is "{}"'.format(btn.state)
+#     # Debug Area
+#     BoxLayout:
+#         orientation: 'horizontal'
+#         padding: '30dp', '20dp'
+#         spacing: '20dp'
+#         Button:
+#             text: 'Exit'
+#             on_press:
+#                 # app.stop()
+#                 exit()
+#         Button:
+#             text: 'Draw Logo'
+#             # on_press: app.root.ids.disp.drawLogo()
+#         Button:
+#             text: 'Clear Display'
+#             on_press:
+#                 on_press: root.ids.disp.clear()
+#         Button:
+#             text: 'DUMP'
+#             on_press:
+#                 # label.text = 'The button was pressed'
+#                 # print('app', app) # CRASH
+#                 # print('app.root', app.root) # CRASH
+#                 print('root.ids', root.ids)
+#                 print('self.ids', self.ids)
+#                 print('self.parent.ids', self.parent.ids)
+#             on_release:
+#                 # label.text = 'The button was released'
+#                 print('The button was released')
+#         Button:
+#             id: btn
+#             text: 'Press me'
+#     BoxLayout:
+#         Label:
+#             id: label
+#             text: 'Button is "{}"'.format(btn.state)
 '''
 
 class EuroPiLayout(BoxLayout):
@@ -138,7 +150,65 @@ class CanvasCvIn(Widget):
 
 class Display(Widget):
     def clear(self):
-        self.canvas.clear()
+        # self.canvas.clear()
+        # workaround
+        texture = Texture.create(size=(128, 32))
+        size = 128 * 32 * 3
+        buf = [255 for x in range(size)]
+        buf = bytes(buf)
+        texture.blit_buffer(buf, colorfmt='rgb', bufferfmt='ubyte')
+        with self.canvas:
+            Rectangle(texture=texture, pos=self.pos, size=(128, 32))
+
+    def drawLogo(self):
+        image = b"\x00\x00\x00\x01\xf0\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x02\x08\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x04\x04\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\xc4\x04\x00\x18\x00\x00\x00p\x07\x00\x00\x00\x00\x00\x00\x0c$\x02\x00~\x0c\x18\xb9\x8c8\xc3\x00\x00\x00\x00\x00\x10\x14\x01\x00\xc3\x0c\x18\xc3\x060c\x00\x00\x00\x00\x00\x10\x0b\xc0\x80\x81\x8c\x18\xc2\x020#\x00\x00\x00\x00\x00 \x04\x00\x81\x81\x8c\x18\x82\x02 #\x00\x00\x00\x00\x00A\x8a|\x81\xff\x0c\x18\x82\x02 #\x00\x00\x00\x00\x00FJC\xc1\x80\x0c\x18\x82\x02 #\x00\x00\x00\x00\x00H\x898\x00\x80\x0c\x18\x83\x060c\x00\x00\x00\x00\x00S\x08\x87\x00\xc3\x060\x81\x8c8\xc3\x00\x00\x00\x00\x00d\x08\x00\xc0<\x01\xc0\x80p7\x03\x00\x00\x00\x00\x00X\x08p \x00\x00\x00\x00\x000\x00\x00\x00\x00\x00\x00#\x88H \x00\x00\x00\x00\x000\x00\x00\x00\x00\x00\x00L\xb8& \x00\x00\x00\x00\x000\x00\x00\x00\x00\x00\x00\x91P\x11 \x00\x00\x00\x00\x000\x00\x00\x00\x00\x00\x00\xa6\x91\x08\xa0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc9\x12\x84`\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x12\x12C\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00$\x11 \x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00H\x0c\x90\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00@\x12\x88\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00 \x12F\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10\x10A\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x10  \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x08  \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x04@@\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc6\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x008\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+        TH = bytearray(image)
+        fb = FrameBuffer(TH, 128, 32, MONO_HLSB)
+        # oled.blit(fb, 0, 0)
+
+        filename = 'software/contrib/andy/temp.xbm'
+        convert_to_xbm(fb, filename)
+
+        # importing image class from PIL package
+        # creating image object
+        image = Image.open(filename)
+        print(image, type(image), image.size, image.mode)
+        # convert to rgb
+        image_rgb = image.convert('RGB')
+        image_rgb = ImageOps.invert(image_rgb)
+        # print(image_rgb, type(image_rgb), image_rgb.size, image_rgb.mode)
+
+        # canvas_img = Image.new('RGB', (240, 120), color=(255, 255, 255))
+        # (do stuff to canvas_img)
+        data = BytesIO()
+        image_rgb.save(data, format='png')
+        data.seek(0) # yes you actually need this
+        im = CoreImage(BytesIO(data.read()), ext='png')
+        # self.beeld = kiImage() # only use this line in first code instance
+        # self.beeld.texture = im.texture       
+        texture = im.texture
+        # texture.flip_vertical()  # not necessary, but does work
+        with self.canvas:
+            # Color(1,1,1,1) # doesn't seem necessary
+            pos = self.pos
+            # pos = (pos[0] + 5, pos[1] + 5)
+            Rectangle(texture=texture, pos=pos, size=(128, 32))
+
+        # Drawing text
+
+        mylabel = CoreLabel(text="Simulator", font_size=11, color=(0, 0, 0, 1))
+        # Force refresh to compute things and generate the texture
+        mylabel.refresh()
+        # Get the texture and the texture size
+        texture = mylabel.texture
+        texture_size = list(texture.size)
+        # Draw the texture on any widget canvas
+        # myWidget = self
+        # myWidget.canvas.add(Rectangle(texture=texture, size=texture_size))
+        with self.canvas:
+            pos = self.pos
+            pos = (pos[0] + 55, pos[1] + 2)
+            Rectangle(texture=texture, pos=pos, size=texture_size)
 
 async def run_app_happily(root, other_task):
     '''This method, which runs Kivy, is run by the asyncio loop as one of the
